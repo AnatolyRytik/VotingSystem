4ec903af93ba67face1a1d70c963f0ce4e6acb13
Anatoly <anatolyrytik@gmail.com>
2020-10-29 18:11:45 +0100

    vote model added

--- src/main/java/topjava/graduation/model/Vote.java
+++ src/main/java/topjava/graduation/model/Vote.java
@@ -1,11 +1,82 @@
 package topjava.graduation.model;
 
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
-public class Vote {
+@Entity
+@Table(name = "vote_restaurant", uniqueConstraints = {@UniqueConstraint(columnNames = {"user_id", "vote_date"}, name = "user_vote_date_idx")})
+public class Vote extends AbstractBaseEntity {
 
-    private LocalDate date;
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @NotNull
     private User user;
+
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "restaurant_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @NotNull
     private Restaurant restaurant;
 
+
+    @Column(name = "vote_date", unique = true)
+    @NotNull
+    private LocalDate date;
+
+
+    public Vote() {
+    }
+
+    public Vote(Long id, User user, Restaurant restaurant, LocalDate date) {
+        super(id);
+        this.user = user;
+        this.restaurant = restaurant;
+        this.date = date;
+    }
+
+    public Vote(User user, Restaurant restaurant, LocalDate date) {
+        this.user = user;
+        this.restaurant = restaurant;
+        this.date = date;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public Restaurant getRestaurant() {
+        return restaurant;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public void setRestaurant(Restaurant restaurant) {
+        this.restaurant = restaurant;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return "Vote{" +
+                "id=" + id +
+                ", user=" + user +
+                ", restaurant=" + restaurant +
+                ", date=" + date +
+                '}';
+    }
 }
