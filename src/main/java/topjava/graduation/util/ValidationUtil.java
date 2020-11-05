b045c40f7e99f873ce8f5cb19c87e0cfe8ee775d
Anatoly <anatolyrytik@gmail.com>
2020-11-05 19:21:46 +0100

    ValidationUtil and NotFoundException

---
+++ src/main/java/topjava/graduation/util/ValidationUtil.java
@@ -0,0 +1,48 @@
+package topjava.graduation.util;
+
+import topjava.graduation.model.AbstractBaseEntity;
+import topjava.graduation.util.exception.NotFoundException;
+
+
+public class ValidationUtil {
+    private ValidationUtil() {
+    }
+
+    public static <T> T checkNotFoundWithId(T object, long id) {
+        checkNotFoundWithId(object != null, id);
+        return object;
+    }
+
+    public static void checkNotFoundWithId(boolean found, long id) {
+        checkNotFound(found, "id=" + id);
+    }
+
+    public static <T> T checkNotFound(T object, String msg) {
+        checkNotFound(object != null, msg);
+        return object;
+    }
+
+    public static void checkNotFound(boolean found, String msg) {
+        if (!found) {
+            throw new NotFoundException("Not found entity with " + msg);
+        }
+    }
+
+    public static void checkNew(AbstractBaseEntity entity) {
+        if (!entity.isNew()) {
+            throw new IllegalArgumentException(entity + " must be new (id=null)");
+        }
+    }
+
+
+    //  http://stackoverflow.com/a/28565320/548473
+    public static Throwable getRootCause(Throwable t) {
+        Throwable result = t;
+        Throwable cause;
+
+        while (null != (cause = result.getCause()) && (result != cause)) {
+            result = cause;
+        }
+        return result;
+    }
+}
