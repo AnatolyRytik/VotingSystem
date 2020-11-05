39e4d79a3252b6c405c9b43c41073d68fbbf82e9
Anatoly <anatolyrytik@gmail.com>
2020-11-05 19:20:57 +0100

    Restaurant repository implementantion

---
+++ src/main/java/topjava/graduation/repository/RestaurantRepository.java
@@ -0,0 +1,33 @@
+package topjava.graduation.repository;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+import topjava.graduation.model.Restaurant;
+
+import java.util.List;
+
+@Transactional(readOnly = true)
+public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
+
+    @Query("SELECT r FROM Restaurant r ORDER BY r.name")
+    List<Restaurant> getAll();
+
+    @Transactional
+    @Override
+    Restaurant save(Restaurant restaurant);
+
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM Restaurant r WHERE r.id=:id")
+    int delete(@Param("id") long id);
+
+    @Query("SELECT r FROM Restaurant r WHERE r.name LIKE CONCAT('%', :name, '%')")
+    List<Restaurant> getByName(@Param("title") String title);
+
+    @Query("SELECT DISTINCT r FROM Restaurant r JOIN FETCH r.lunches ORDER BY r.name")
+    List<Restaurant> getRestaurantsWithLunches();
+}
